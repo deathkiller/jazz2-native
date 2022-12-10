77aac7e7d2950a7529f56e89ca4280b626af3c07
Death Killer <danielrajf@gmail.com>
2022-12-10 20:48:17 +0100

    Fixed RollingRock

--- Sources/Jazz2/Actors/Environment/RollingRock.cpp
+++ Sources/Jazz2/Actors/Environment/RollingRock.cpp
@@ -9,8 +9,6 @@ namespace Jazz2::Actors::Environment
 		:
 		_delayLeft(40.0f),
 		_triggered(false),
-		_speedUpDirection(SpeedUpDirection::None),
-		_speedUpDirectionCooldown(0.0f),
 		_soundCooldown(0.0f)
 	{
 	}
@@ -23,13 +21,15 @@ namespace Jazz2::Actors::Environment
 		_triggerSpeedX = (float)*(int8_t*)&details.Params[1];
 		_triggerSpeedY = (float)*(int8_t*)&details.Params[2];
 
-		SetState(ActorState::IsInvulnerable, true);
+		SetState(ActorState::CollideWithSolidObjects | ActorState::CollideWithSolidObjectsBelow | ActorState::IsSolidObject | ActorState::IsInvulnerable, true);
 		SetState(ActorState::ApplyGravitation, false);
 		_elasticity = 0.4f;
 		_canHurtPlayer = false;
 
 		async_await RequestMetadataAsync("Object/RollingRock"_s);
 		SetAnimation(AnimState::Idle);
+
+		_renderer.setRotation(_pos.X + _pos.Y);
 
 		async_return true;
 	}
@@ -38,119 +38,66 @@ namespace Jazz2::Actors::Environment
 	{
 		UpdateFrozenState(timeMult);
 
+		if (_soundCooldown > 0.0f) {
+			_soundCooldown -= timeMult;
+		}
+
 		if (_frozenTimeLeft > 0.0f) {
 			return;
 		}
 
-		// Movement
 		if (GetState(ActorState::ApplyGravitation)) {
+			// Rock is triggered
 			float currentGravity = _levelHandler->Gravity;
-			float accelY = (_internalForceY + _externalForce.Y) * timeMult;
 
-			_speed.X = std::clamp(_speed.X, -16.0f, 16.0f);
-			_speed.Y = std::clamp(_speed.Y + accelY, -16.0f, 16.0f);
+			_speed.X = std::clamp(_speed.X + _externalForce.X * timeMult, -3.0f, 3.0f) * powf(0.6f, timeMult);
 
-			float effectiveSpeedX = (_speed.X + _externalForce.X) * timeMult * timeMult;
-			float effectiveSpeedY = (_speed.Y + 0.5f * accelY) * timeMult;
+			bool up = IsPositionEmpty(_pos.X, _pos.Y - 33.0f);
+			bool down = IsPositionEmpty(_pos.X - 4.0f, _pos.Y + 36.0f) && IsPositionEmpty(_pos.X + 4.0f, _pos.Y + 36.0f);
+			bool left = IsPositionEmpty(_pos.X - 36.0f, _pos.Y) && IsPositionEmpty(_pos.X - 19.0f, _pos.Y - 19.0f) && IsPositionEmpty(_pos.X - 4.0f, _pos.Y + 33.0f);
+			bool right = IsPositionEmpty(_pos.X + 36.0f, _pos.Y) && IsPositionEmpty(_pos.X + 19.0f, _pos.Y + 19.0f) && IsPositionEmpty(_pos.X + 4.0f, _pos.Y + 33.0f);
 
-			bool success = false;
-
-			float maxYDiff = std::max(3.0f, std::abs(effectiveSpeedX) + 2.5f);
-			float yDiff;
-			for (yDiff = maxYDiff + effectiveSpeedY; yDiff >= -maxYDiff + effectiveSpeedY; yDiff -= CollisionCheckStep) {
-				if (MoveInstantly(Vector2f(effectiveSpeedX, yDiff), MoveType::Relative)) {
-					success = true;
-					if (yDiff <= maxYDiff + effectiveSpeedY * 0.8f) {
-						_speed.X += yDiff * (_speed.X < 0.0f ? -0.02f : 0.02f);
-					}
-					break;
+			if (down) {
+				if (_pos.Y > _levelHandler->WaterLevel()) {
+					_speed.Y += currentGravity * 0.25f * timeMult;
+				} else {
+					_speed.Y += currentGravity * timeMult;
 				}
 			}
 
-			// Also try to move horizontally as far as possible
-			float xDiff = std::abs(effectiveSpeedX);
-			float maxXDiff = -xDiff;
-			if (!success) {
-				int sign = (effectiveSpeedX > 0.0f ? 1 : -1);
-				for (; xDiff >= maxXDiff; xDiff -= CollisionCheckStep) {
-					if (MoveInstantly(Vector2f(xDiff * sign, 0.0f), MoveType::Relative)) {
-						break;
-					}
-				}
-
-				// If no angle worked in the previous step, the actor is facing a wall
-				if (xDiff > CollisionCheckStep || (xDiff > 0.0f && _elasticity > 0.0f)) {
-					_speed.X = -(_elasticity * _speed.X);
-					_speedUpDirection = SpeedUpDirection::None;
-					_speedUpDirectionCooldown = 60.0f;
-
-					if (_soundCooldown <= 0.0f) {
-						_soundCooldown = 140.0f;
-						PlaySfx("Hit"_s, 0.6f, 0.4f);
-					}
-				}
-			}
-
-			if (yDiff > 0.0f) {
-				yDiff = std::min(8.0f, yDiff * yDiff);
-			}
-
-			_speed.X += (_speed.X < 0.0f ? -1.0f : 1.0f) * (yDiff * 0.02f - 0.02f) * timeMult;
-
-			if (_speed.Y > 0.0f && yDiff <= 0.0f) {
-				_speed.Y = 0.0f;
-
-				if (_soundCooldown <= 0.0f) {
+			//int hits = 0;
+			if ((_speed.X < 0.0f && !left) || (_speed.X > 0.0f && !right)) {
+				// Bounce on X
+				if (_soundCooldown <= 0.0f && std::abs(_speed.X) > 2.0f) {
 					_soundCooldown = 140.0f;
 					PlaySfx("Hit"_s, 0.6f, 0.4f);
 				}
+
+				_speed.X = _speed.X * -0.5f;
+				_externalForce.X = _externalForce.X * -0.5f;
+				//hits |= 0x1;
+			}
+			if ((_speed.Y < 0.0f && !up) || (_speed.Y > 0.0f && !down)) {
+				// Bounce on Y
+				if (_soundCooldown <= 0.0f && std::abs(_speed.Y) > 2.0f) {
+					_soundCooldown = 140.0f;
+					PlaySfx("Hit"_s, 0.6f, 0.4f);
+				}
+
+				_speed.Y = _speed.Y * -0.5f;
+				//hits |= 0x2;
 			}
 
-			if (std::abs(_speed.X) < 0.4f) {
-				if (_speedUpDirectionCooldown > 0.0f) {
-					_speedUpDirection = SpeedUpDirection::None;
-					_speed.X = 0.0f;
-					_speed.Y = 0.0f;
-				} else if (_speedUpDirection == SpeedUpDirection::Left) {
-					_speed.X = -0.6f;
-					_speedUpDirection = SpeedUpDirection::None;
-					_speedUpDirectionCooldown = 60.0f;
-				} else if (_speedUpDirection == SpeedUpDirection::Right) {
-					_speed.X = 0.6f;
-					_speedUpDirection = SpeedUpDirection::None;
-					_speedUpDirectionCooldown = 60.0f;
-				}
-			} else if (yDiff < -1.0f) {
-				if (_speed.X > 0.0f && _speedUpDirection != SpeedUpDirection::Left) {
-					_speedUpDirection = SpeedUpDirection::Left;
-				} else if (_speed.X < 0.0f && _speedUpDirection != SpeedUpDirection::Right) {
-					_speedUpDirection = SpeedUpDirection::Right;
-				}
-			} else {
-				_speedUpDirectionCooldown -= timeMult;
-			}
+			MoveInstantly(Vector2f(_speed.X * timeMult, _speed.Y * timeMult), MoveType::Relative | MoveType::Force);
 
-			// Reduce all forces if they are present
-			if (std::abs(_externalForce.X) > 0.0f) {
-				if (_externalForce.X > 0.0f) {
-					_externalForce.X = std::max(_externalForce.X - _friction * timeMult, 0.0f);
-				} else {
-					_externalForce.X = std::min(_externalForce.X + _friction * timeMult, 0.0f);
-				}
-			}
-			_externalForce.Y = std::min(_externalForce.Y + currentGravity * 0.33f * timeMult, 0.0f);
-			_internalForceY = std::min(_internalForceY + currentGravity * 0.33f * timeMult, 0.0f);
-
-			_renderer.setRotation(_renderer.rotation() + effectiveSpeedX * 0.02f * timeMult);
-
-			if (std::abs(_speed.X) <= 0.4f * timeMult && std::abs(_speed.Y) <= 0.4f * timeMult) {
-				SetState(ActorState::CollideWithOtherActors | ActorState::ApplyGravitation, false);
+			if (std::abs(_speed.X) < 0.2f && !down) {
+				_speed = Vector2f::Zero;
+				_externalForce = Vector2f::Zero;
+				SetState(ActorState::ApplyGravitation, false);
 				_canHurtPlayer = false;
 			}
 
-			if (_soundCooldown > 0.0f) {
-				_soundCooldown -= timeMult;
-			}
+			_renderer.setRotation(_renderer.rotation() + _speed.X * 0.02f * timeMult);
 		}
 
 		// Trigger
@@ -161,8 +108,9 @@ namespace Jazz2::Actors::Environment
 				SetState(ActorState::ApplyGravitation, true);
 				_canHurtPlayer = true;
 
-				_externalForce.X = _triggerSpeedX * 0.5f;
-				_externalForce.Y = _triggerSpeedY * -0.5f;
+				_speed.X = _triggerSpeedX;
+				_speed.Y = _triggerSpeedY;
+				_externalForce.X = (_triggerSpeedX < 0.0f ? -4.0f : 4.0f);
 
 				if (_soundCooldown <= 0.0f) {
 					_soundCooldown = 140.0f;
@@ -174,14 +122,89 @@ namespace Jazz2::Actors::Environment
 
 	void RollingRock::OnUpdateHitbox()
 	{
-		UpdateHitbox(30, 30);
+		UpdateHitbox(50, 50);
+	}
+
+	bool RollingRock::OnHandleCollision(std::shared_ptr<ActorBase> other)
+	{
+		if (auto rollingRock = dynamic_cast<RollingRock*>(other.get())) {
+			float dx = (rollingRock->_pos.X - _pos.X);
+			float dy = (rollingRock->_pos.Y - _pos.Y);
+			float distance = Vector2f(dx, dy).Length();
+			if (distance < 74.0f) {
+				float relSpeeds = (std::abs(_speed.X) + std::abs(rollingRock->_speed.X)) * 0.5f;
+
+				if (dx > 0.0f) {
+					_speed.X = -relSpeeds;
+					_externalForce.X = -std::abs(_externalForce.X);
+				} else {
+					_speed.X = relSpeeds;
+					_externalForce.X = std::abs(_externalForce.X);
+				}
+				if (dx < 0.0f) {
+					rollingRock->_speed.X = -relSpeeds;
+					rollingRock->_externalForce.X = -std::abs(rollingRock->_externalForce.X);
+				} else {
+					rollingRock->_speed.X = relSpeeds;
+					rollingRock->_externalForce.X = std::abs(rollingRock->_externalForce.X);
+				}
+
+				if (relSpeeds > 1.0f) {
+					_triggered = true;
+				}
+
+				SetState(ActorState::CanBeFrozen, true);
+			}
+			return true;
+		} else if (auto player = dynamic_cast<Player*>(other.get())) {
+			if (_triggered) {
+				float dx = (player->GetPos().X - _pos.X);
+				float dy = (player->GetPos().Y - _pos.Y);
+				float distance = Vector2f(dx, dy).Length();
+
+				if (distance < 50.0f || dy > 0.0f) {
+					float relSpeeds = (std::abs(_speed.X) + std::abs(player->GetSpeed().X)) * 0.5f;
+					if (relSpeeds < 1.0f) {
+						relSpeeds = (60.0f - distance) * 0.2f;
+					}
+
+					if (dx > 0.0f) {
+						_speed.X = -2.0f;
+						_externalForce.X = -std::abs(_externalForce.X);
+					} else {
+						_speed.X = 2.0f;
+						_externalForce.X = std::abs(_externalForce.X);
+					}
+					if (dy > 0.0f) {
+						_externalForce.Y -= 2.0f;
+					}
+					SetState(ActorState::ApplyGravitation, true);
+				}
+			}
+		}
+
+		return EnemyBase::OnHandleCollision(other);
 	}
 
 	void RollingRock::OnTriggeredEvent(EventType eventType, uint8_t* eventParams)
 	{
 		if (!_triggered && eventType == EventType::RollingRockTrigger && eventParams[0] == _id) {
 			_triggered = true;
+
 			_levelHandler->ShakeCameraView(60.0f);
+
+			// TODO: Shake amplitude
+			/*float distanceToPlayer;
+			if (distanceToPlayer < 400.0f) {
+				float amplitude = std::min((400.0f - distanceToPlayer) / (256.0f * 8.0f), 40.0f);
+				_levelHandler->ShakeCameraView(amplitude);
+			}*/
 		}
+	}
+
+	bool RollingRock::IsPositionEmpty(float x, float y)
+	{
+		TileCollisionParams params = { TileDestructType::None, _speed.Y >= 0.0f };
+		return _levelHandler->IsPositionEmpty(this, AABBf(x - 1.0f, y - 1.0f, x + 1.0f, y + 1.0f), params);
 	}
 }
