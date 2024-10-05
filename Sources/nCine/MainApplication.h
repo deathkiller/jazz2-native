36d324f812015b13f4c3a98f67fe1ed7a04f49e7
Death Killer <884052+deathkiller@users.noreply.github.com>
2024-10-06 01:13:14 +0200

    Reworked logging and exception handling

--- Sources/nCine/MainApplication.h
+++ Sources/nCine/MainApplication.h
@@ -14,35 +14,29 @@ namespace nCine
 	class Qt5Widget;
 #endif
 
-	/// Handler class for nCine applications on PC
 	class MainApplication : public Application
 	{
 	public:
-		/**  @brief Entry point method to be called in the `main()` function */ 
+		/** @brief Entry point method to be called in the `main()`/`wWinMain()` function */ 
 		static int Run(std::unique_ptr<IAppEventHandler>(*createAppEventHandler)(), int argc, NativeArgument* argv);
 
-		void AttachTraceTarget(Containers::StringView targetPath) override;
-
 	private:
-		/// Suspension state from last frame
 		bool wasSuspended_;
 
 #if defined(WITH_QT5)
-		/// A pointer to the custom Qt5 widget
 		Qt5Widget* qt5Widget_;
 #endif
 
-		/// Must be called at the beginning to initialize the application
+		/** @brief Must be called at the beginning to initialize the application */
 		void Init(std::unique_ptr<IAppEventHandler>(*createAppEventHandler)(), int argc, NativeArgument* argv);
-		/// Must be called continuously to keep the application running
+		/** @brief Must be called continuously to keep the application running */
 		void ProcessStep();
-		/// Processes events inside the game loop
+		/** @brief Processes events inside the game loop */
 		void ProcessEvents();
 #if defined(DEATH_TARGET_EMSCRIPTEN)
 		static void EmscriptenStep();
 #endif
 
-		/// Private constructor
 		MainApplication()
 			:
 			Application(),
@@ -64,7 +58,6 @@ namespace nCine
 #endif
 	};
 
-	/// Meyers' Singleton
 	Application& theApplication();
 
 }
