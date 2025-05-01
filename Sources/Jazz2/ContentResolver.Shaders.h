8c3640c40dc6089b34292e8ddf58e93176bfb3d2
Death Killer <884052+deathkiller@users.noreply.github.com>
2025-05-01 14:20:58 +0200

    Trying to fix shaders on Mali GPU (part 2)

--- Sources/Jazz2/ContentResolver.Shaders.h
+++ Sources/Jazz2/ContentResolver.Shaders.h
@@ -348,7 +348,7 @@ void main() {
 	vec4 main = texture(uTexture, uv);
 
 	// Chromatic Aberration
-	/*float aberration = abs(uvLocal.x - 0.5) * 0.012;
+	float aberration = abs(uvLocal.x - 0.5) * 0.012;
 	float red = texture(uTexture, vec2(uv.x - aberration, uv.y)).r;
 	float blue = texture(uTexture, vec2(uv.x + aberration, uv.y)).b;
 	main.rgb = mix(main.rgb, waterColor * (0.4 + 1.2 * vec3(red, main.g, blue)), vec3(isTexelBelow * 0.5));
@@ -359,7 +359,7 @@ void main() {
 	main.rgb += vec3(rays * isTexelBelow * max(1.0 - uvLocal.y * 1.4, 0.0) * 0.6);
 	
 	// Waves
-	float topDist = abs(uvLocal.y - uWaterLevel - waveHeight);
+	/*float topDist = abs(uvLocal.y - uWaterLevel - waveHeight);
 	float isNearTop = 1.0 - aastep(vViewSizeInv.y * 2.8, topDist);
 	float isVeryNearTop = 1.0 - aastep(vViewSizeInv.y * (0.8 - 100.0 * waveHeight), topDist);
 
