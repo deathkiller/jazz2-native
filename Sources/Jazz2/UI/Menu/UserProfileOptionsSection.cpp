28e17d1f42cbb3e7c71f926a1cbeefa0c8e54a55
Death Killer <884052+deathkiller@users.noreply.github.com>
2025-07-09 20:32:37 +0200

    Fixed `Dragon` enemy

--- Sources/Jazz2/UI/Menu/UserProfileOptionsSection.cpp
+++ Sources/Jazz2/UI/Menu/UserProfileOptionsSection.cpp
@@ -425,7 +425,7 @@ namespace Jazz2::UI::Menu
 			case UserProfileOptionsItemType::UniquePlayerID: {
 				auto& uuid = PreferencesCache::UniquePlayerID;
 				char uniquePlayerId[128];
-				std:size_t length = formatInto(uniquePlayerId, "{:.2X}{:.2X}:{:.2X}{:.2X}:{:.2X}{:.2X}:{:.2X}{:.2X}:{:.2X}{:.2X}:{:.2X}{:.2X}:{:.2X}{:.2X}:{:.2X}{:.2X}",
+				std::size_t length = formatInto(uniquePlayerId, "{:.2X}{:.2X}:{:.2X}{:.2X}:{:.2X}{:.2X}:{:.2X}{:.2X}:{:.2X}{:.2X}:{:.2X}{:.2X}:{:.2X}{:.2X}:{:.2X}{:.2X}",
 					uuid[0], uuid[1], uuid[2], uuid[3], uuid[4], uuid[5], uuid[6], uuid[7], uuid[8], uuid[9], uuid[10], uuid[11], uuid[12], uuid[13], uuid[14], uuid[15]);
 
 				if (theApplication().GetInputManager().setClipboardText({ uniquePlayerId, length })) {
