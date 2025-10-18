4fbd647bdbbd8b9599d730965fdf222619f223db
Death Killer <884052+deathkiller@users.noreply.github.com>
2025-10-18 11:24:42 +0200

    Fixed build

--- Sources/nCine/Graphics/TextureLoaderPkm.cpp
+++ Sources/nCine/Graphics/TextureLoaderPkm.cpp
@@ -19,16 +19,16 @@ namespace nCine
 		fileHandle_->Read(&header, 16); // PKM header is 16 bytes long
 
 		// Checking for the header presence
-		DEATH_ASSERT(Stream::Uint32FromBE(header.magicId) == 0x504B4D20 /* "PKM 10" */, "Invalid PKM signature", );
-		DEATH_ASSERT(Stream::Uint16FromBE(header.version) == 0x3130 /* "10" */, ("PKM version not supported: 0x{:.4x}", header.version), );
-		DEATH_ASSERT(Stream::Uint16FromBE(header.dataType) == 0, ("PKM data type not supported: 0x{:.4x}", header.dataType), );
+		DEATH_ASSERT(Stream::FromBE(header.magicId) == 0x504B4D20 /* "PKM 10" */, "Invalid PKM signature", );
+		DEATH_ASSERT(Stream::FromBE(header.version) == 0x3130 /* "10" */, ("PKM version not supported: 0x{:.4x}", header.version), );
+		DEATH_ASSERT(Stream::FromBE(header.dataType) == 0, ("PKM data type not supported: 0x{:.4x}", header.dataType), );
 
 		headerSize_ = 16;
-		width_ = Stream::Uint16FromBE(header.width);
-		height_ = Stream::Uint16FromBE(header.height);
+		width_ = Stream::FromBE(header.width);
+		height_ = Stream::FromBE(header.height);
 
-		const int extWidth = Stream::Uint16FromBE(header.extendedWidth);
-		const int extHeight = Stream::Uint16FromBE(header.extendedHeight);
+		const int extWidth = Stream::FromBE(header.extendedWidth);
+		const int extHeight = Stream::FromBE(header.extendedHeight);
 
 		LOGI("Header found: w:{} h:{}, xw:{} xh:{}", width_, height_, extWidth, extHeight);
 
